49c8ce05e9139af787cd5bfd2d1382cded106126
Marija <maria5stojanovic@gmail.com>
2021-03-22 17:22:30 -0400

    setup for the app

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Aux from "../../hoc/Aux";
+
+function layout = (props) =>(
+  <Aux>
+  <div>Toolbar</div>;
+  <main>{props.children}</main>;
+  </Aux>
+);
+export default layout;
+
