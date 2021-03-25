f5910878b25fcbfb807beddaad8aaef862c3c957
Marija <maria5stojanovic@gmail.com>
2021-03-24 22:48:18 -0400

    add controls for making a burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,14 +1,22 @@
+import { array } from "prop-types";
 import React from "react";
 import classes from "./Burger.module.css";
 
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients).map((ing) => {
-    return [...Array(props.ingredients[ing])].map((_, i) => {
-      return <BurgerIngredient key={ing + 1} type={ing} />;
-    });
-  });
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((ing) => {
+      return [...Array(props.ingredients[ing])].map((_, i) => {
+        return <BurgerIngredient key={ing + 1} type={ing} />;
+      });
+    })
+    .reduce((array, el) => {
+      return array.concat(el);
+    }, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
