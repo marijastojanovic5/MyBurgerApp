33ce70fc13d36ded2f7b811c875d127c3420e236
Marija <maria5stojanovic@gmail.com>
2021-03-27 19:24:25 -0400

    add functionality for ADD button

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,7 +5,9 @@ const buildControl = (props) => (
   <div className={classes.BuildControl}>
     <div classname={classes.Label}>{props.label}</div>
     <button className={classes.Less}>Less</button>
-    <button className={classes.More}>More</button>
+    <button className={classes.More} onClick={props.added}>
+      More
+    </button>
   </div>
 );
 
